28504b13885007497507d1210a84229f414d4a55
Peter Do <peterdo3r7@gmail.com>
2021-01-25 22:22:54 -0500

    Initial project add

---
+++ src/main/java/guru/springframework/sgpetcare/SgPetCareApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sgpetcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SgPetCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SgPetCareApplication.class, args);
+    }
+
+}
